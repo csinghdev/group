40dbb0eaea8deacd8093b395ed21b82b545f7ee6
Chandresh <chandresh989@gmail.com>
2015-12-07 02:21:26 +0530

    added responsive views for email

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,16 +1,360 @@
-<!DOCTYPE html>
-<html lang="en-US">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8">
-</head>
-<body>
-<h4>Hi {{ $username }}</h4>
-<br>
-<div>
-    Thanks for creating an account on MyGroup.<br>
-    Please follow the link below to verify your email address
-    {{ URL::to('api/v1/register/'. $email . '/verify/' . $confirmation_code) }}.<br/>
-</div>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>My Group</title>
+    <style type="text/css">
+        /* /\/\/\/\/\/\/\/\/ CLIENT-SPECIFIC STYLES /\/\/\/\/\/\/\/\/ */
+        #outlook a{padding:0;} /* Force Outlook to provide a "view in browser" message */
+        .ReadMsgBody{width:100%;} .ExternalClass{width:100%;} /* Force Hotmail to display emails at full width */
+        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;} /* Force Hotmail to display normal line spacing */
+        body, table, td, p, a, li, blockquote{-webkit-text-size-adjust:100%; -ms-text-size-adjust:100%;} /* Prevent WebKit and Windows mobile changing default text sizes */
+        table, td{mso-table-lspace:0pt; mso-table-rspace:0pt;} /* Remove spacing between tables in Outlook 2007 and up */
+        img{-ms-interpolation-mode:bicubic;} /* Allow smoother rendering of resized image in Internet Explorer */
 
+        /* /\/\/\/\/\/\/\/\/ RESET STYLES /\/\/\/\/\/\/\/\/ */
+        body{margin:0; padding:0;}
+        img{border:0; height:auto; line-height:100%; outline:none; text-decoration:none;}
+        table{border-collapse:collapse !important;}
+        body, #bodyTable, #bodyCell{height:100% !important; margin:0; padding:0; width:100% !important;}
+
+        /* /\/\/\/\/\/\/\/\/ TEMPLATE STYLES /\/\/\/\/\/\/\/\/ */
+
+        /* ========== Page Styles ========== */
+
+        #bodyCell{padding:20px;}
+        #templateContainer{width:600px;}
+
+        body, #bodyTable{
+            /*@editable*/ background-color:#f5f5dc;
+        }
+
+
+        #bodyCell{
+            /*@editable*/ border-top:4px solid #1f99f9;
+        }
+
+
+        #templateContainer{
+            /*@editable*/ border:1px solid #BBBBBB;
+        }
+
+
+        h1{
+            /*@editable*/ color:#202020 !important;
+            display:block;
+            /*@editable*/ font-family:Helvetica;
+            /*@editable*/ font-size:26px;
+            /*@editable*/ font-style:normal;
+            /*@editable*/ font-weight:bold;
+            /*@editable*/ line-height:100%;
+            /*@editable*/ letter-spacing:normal;
+            margin-top:0;
+            margin-right:0;
+            margin-bottom:10px;
+            margin-left:0;
+            /*@editable*/ text-align:left;
+        }
+
+
+        h2{
+            /*@editable*/ color:#404040 !important;
+            display:block;
+            /*@editable*/ font-family:Helvetica;
+            /*@editable*/ font-size:20px;
+            /*@editable*/ font-style:normal;
+            /*@editable*/ font-weight:bold;
+            /*@editable*/ line-height:100%;
+            /*@editable*/ letter-spacing:normal;
+            margin-top:0;
+            margin-right:0;
+            margin-bottom:10px;
+            margin-left:0;
+            /*@editable*/ text-align:left;
+        }
+
+        h3{
+            /*@editable*/ color:#606060 !important;
+            display:block;
+            /*@editable*/ font-family:Helvetica;
+            /*@editable*/ font-size:16px;
+            /*@editable*/ font-style:italic;
+            /*@editable*/ font-weight:normal;
+            /*@editable*/ line-height:100%;
+            /*@editable*/ letter-spacing:normal;
+            margin-top:0;
+            margin-right:0;
+            margin-bottom:10px;
+            margin-left:0;
+            /*@editable*/ text-align:left;
+        }
+
+
+        h4{
+            /*@editable*/ color:#808080 !important;
+            display:block;
+            /*@editable*/ font-family:Helvetica;
+            /*@editable*/ font-size:14px;
+            /*@editable*/ font-style:italic;
+            /*@editable*/ font-weight:normal;
+            /*@editable*/ line-height:100%;
+            /*@editable*/ letter-spacing:normal;
+            margin-top:0;
+            margin-right:0;
+            margin-bottom:10px;
+            margin-left:0;
+            /*@editable*/ text-align:left;
+        }
+
+        /* ========== Header Styles ========== */
+
+        #templatePreheader{
+            /*@editable*/ background-color:#F4F4F4;
+            /*@editable*/ border-bottom:1px solid #CCCCCC;
+        }
+
+        .preheaderContent{
+            /*@editable*/ color:#808080;
+            /*@editable*/ font-family:Helvetica;
+            /*@editable*/ font-size:10px;
+            /*@editable*/ line-height:125%;
+            /*@editable*/ text-align:left;
+        }
+
+        .preheaderContent a:link, .preheaderContent a:visited, /* Yahoo! Mail Override */ .preheaderContent a .yshortcuts /* Yahoo! Mail Override */{
+            /*@editable*/ color:#606060;
+            /*@editable*/ font-weight:normal;
+            /*@editable*/ text-decoration:underline;
+        }
+
+        #templateHeader{
+            /*@editable*/ background-color:#F4F4F4;
+            /*@editable*/ border-top:1px solid #FFFFFF;
+            /*@editable*/ border-bottom:1px solid #CCCCCC;
+        }
+
+        .headerContent{
+            /*@editable*/ color:#505050;
+            /*@editable*/ font-family:Helvetica;
+            /*@editable*/ font-size:20px;
+            /*@editable*/ font-weight:bold;
+            /*@editable*/ line-height:100%;
+            /*@editable*/ padding-top:0;
+            /*@editable*/ padding-right:0;
+            /*@editable*/ padding-bottom:0;
+            /*@editable*/ padding-left:0;
+            /*@editable*/ text-align:left;
+            /*@editable*/ vertical-align:middle;
+        }
+
+        .headerContent a:link, .headerContent a:visited, /* Yahoo! Mail Override */ .headerContent a .yshortcuts /* Yahoo! Mail Override */{
+            /*@editable*/ color:#EB4102;
+            /*@editable*/ font-weight:normal;
+            /*@editable*/ text-decoration:underline;
+        }
+
+        #headerImage{
+            height:auto;
+            max-width:600px;
+        }
+
+        /* ========== Body Styles ========== */
+
+        #templateBody{
+            /*@editable*/ background-color:#FFFFFF;
+            /*@editable*/ border-top:1px solid #FFFFFF;
+            /*@editable*/ border-bottom:1px solid #CCCCC8;
+        }
+
+        .bodyContent{
+            /*@editable*/ color:#505050;
+            /*@editable*/ font-family:Helvetica;
+            /*@editable*/ font-size:14px;
+            /*@editable*/ line-height:150%;
+            padding-top:20px;
+            padding-right:20px;
+            padding-bottom:20px;
+            padding-left:20px;
+            /*@editable*/ text-align:left;
+        }
+
+        .bodyContent a:link, .bodyContent a:visited, /* Yahoo! Mail Override */ .bodyContent a .yshortcuts /* Yahoo! Mail Override */{
+            /*@editable*/ color:#EB4102;
+            /*@editable*/ font-weight:normal;
+            /*@editable*/ text-decoration:underline;
+        }
+
+        .bodyContent img{
+            display:inline;
+            height:auto;
+            max-width:560px;
+        }
+
+        /* ========== Footer Styles ========== */
+
+        #templateFooter{
+            /*@editable*/ background-color:#187ac7;
+            /*@editable*/ border-top:1px solid #156bae;
+        }
+
+        .footerContent{
+            /*@editable*/ color:#FFFFFF;
+            /*@editable*/ font-family:Helvetica;
+            /*@editable*/ font-size:10px;
+            /*@editable*/ line-height:150%;
+            padding-top:20px;
+            padding-right:20px;
+            padding-bottom:20px;
+            padding-left:20px;
+            /*@editable*/ text-align:left;
+        }
+
+        .footerContent a:link, .footerContent a:visited, /* Yahoo! Mail Override */ .footerContent a .yshortcuts, .footerContent a span /* Yahoo! Mail Override */{
+            /*@editable*/ color:#FFFFFF;
+            /*@editable*/ font-weight:normal;
+            /*@editable*/ text-decoration:underline;
+        }
+
+        /* /\/\/\/\/\/\/\/\/ MOBILE STYLES /\/\/\/\/\/\/\/\/ */
+
+        @media only screen and (max-width: 480px){
+            /* /\/\/\/\/\/\/ CLIENT-SPECIFIC MOBILE STYLES /\/\/\/\/\/\/ */
+            body, table, td, p, a, li, blockquote{-webkit-text-size-adjust:none !important;} /* Prevent Webkit platforms from changing default text sizes */
+            body{width:100% !important; min-width:100% !important;} /* Prevent iOS Mail from adding padding to the body */
+
+            /* /\/\/\/\/\/\/ MOBILE RESET STYLES /\/\/\/\/\/\/ */
+            #bodyCell{padding:10px !important;}
+
+            /* /\/\/\/\/\/\/ MOBILE TEMPLATE STYLES /\/\/\/\/\/\/ */
+
+            /* ======== Page Styles ======== */
+
+            #templateContainer{
+                max-width:600px !important;
+                /*@editable*/ width:100% !important;
+            }
+
+            h1{
+                /*@editable*/ font-size:24px !important;
+                /*@editable*/ line-height:100% !important;
+            }
+
+            h2{
+                /*@editable*/ font-size:20px !important;
+                /*@editable*/ line-height:100% !important;
+            }
+
+            h3{
+                /*@editable*/ font-size:18px !important;
+                /*@editable*/ line-height:100% !important;
+            }
+
+            h4{
+                /*@editable*/ font-size:16px !important;
+                /*@editable*/ line-height:100% !important;
+            }
+
+            /* ======== Header Styles ======== */
+
+            #templatePreheader{display:none !important;} /* Hide the template preheader to save space */
+
+            #headerImage{
+                height:auto !important;
+                /*@editable*/ max-width:600px !important;
+                /*@editable*/ width:100% !important;
+            }
+
+            .headerContent{
+                /*@editable*/ font-size:20px !important;
+                /*@editable*/ line-height:125% !important;
+            }
+
+            /* ======== Body Styles ======== */
+
+            .bodyContent{
+                /*@editable*/ font-size:18px !important;
+                /*@editable*/ line-height:125% !important;
+            }
+
+            /* ======== Footer Styles ======== */
+
+            .footerContent{
+                /*@editable*/ font-size:14px !important;
+                /*@editable*/ line-height:115% !important;
+            }
+
+            .footerContent a{display:block !important;} /* Place footer social and utility links on their own lines, for easier access */
+        }
+    </style>
+</head>
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+<center>
+    <table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
+        <tr>
+            <td align="center" valign="top" id="bodyCell">
+                <!-- BEGIN TEMPLATE // -->
+                <table border="0" cellpadding="0" cellspacing="0" id="templateContainer">
+                    <tr>
+                        <td align="center" valign="top">
+                            <!-- BEGIN HEADER // -->
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateHeader">
+                                <tr>
+                                    <td valign="top" class="headerContent">
+                                        <img src="<?php echo $message->embed(public_path()."/images/mgLogo.jpg"); ?>" style="max-width:600px;" id="headerImage" mc:label="header_image" mc:edit="header_image" mc:allowdesigner mc:allowtext />
+                                    </td>
+                                </tr>
+                            </table>
+                            <!-- // END HEADER -->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" valign="top">
+                            <!-- BEGIN BODY // -->
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateBody">
+                                <tr>
+                                    <td valign="top" class="bodyContent" mc:edit="body_content">
+                                        <h1>Welcome to My Group</h1>
+                                        <h3>Hi {{ $username }}</h3>
+                                        Please follow the link below to verify your email address
+                                        <br />
+                                        {{ URL::to('api/v1/register/'. $email . '/verify/' . $confirmation_code) }}.
+                                        <br />
+                                        <br />
+
+                                    </td>
+                                </tr>
+                            </table>
+                            <!-- // END BODY -->
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="center" valign="top">
+                            <!-- BEGIN FOOTER // -->
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%" id="templateFooter">
+                                <tr>
+                                    <td valign="top" class="footerContent" mc:edit="footer_content00">
+                                        <a href="*|TWITTER:PROFILEURL|*">Follow on Twitter</a>&nbsp;&nbsp;&nbsp;<a href="*|FACEBOOK:PROFILEURL|*">Friend on Facebook</a>&nbsp;&nbsp;&nbsp;
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td valign="top" class="footerContent" style="padding-top:0;" mc:edit="footer_content01">
+                                        <em>Copyright &copy; 2016 MyGroup, All rights reserved.</em>
+                                        <br />
+                                        <br />
+                                        <strong>Our mailing address is:</strong>
+                                        <br />
+                                        info@mygroup.me
+                                    </td>
+                                </tr>
+                            </table>
+                            <!-- // END FOOTER -->
+                        </td>
+                    </tr>
+                </table>
+                <!-- // END TEMPLATE -->
+            </td>
+        </tr>
+    </table>
+</center>
 </body>
 </html>
